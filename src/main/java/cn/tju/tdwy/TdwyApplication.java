9ff82c6413bb8766eebada4800aa2a42c0c3e0fe
lq296301852 <296301852@qq.com>
2019-11-25 15:31:15 +0800

    init

---
+++ src/main/java/cn/tju/tdwy/TdwyApplication.java
@@ -0,0 +1,13 @@
+package cn.tju.tdwy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdwyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TdwyApplication.class, args);
+    }
+
+}
---
+++ src/main/java/cn/tju/tdwy/dao/UserMapper.java
@@ -0,0 +1,19 @@
+package cn.tju.tdwy.dao;
+
+import cn.tju.tdwy.daomain.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@Mapper
+public interface UserMapper {
+    List<User> getUserByEmail(@Param("email") String email);
+    List<User> getUserByUsername(@Param("username") String username);
+    List<User> getUserByToken(@Param("token") String token);
+    int insertUser(@Param("user") User user);
+    int deleteUserById(@Param("id") int id);
+    int updateUser(@Param("user") User user);
+}
